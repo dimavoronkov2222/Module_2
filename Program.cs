a1273c512c467ae3b2418c11ff6cb22b46f999a1
dimavoronkov2222@hotmail.com <dimavoronkov2222@hotmail.com>
2023-12-26 22:48:43 +0100

    343432

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,35 @@ namespace Module_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string connectionString = "Data Source=LAPTOP-2;Initial Catalog=Warehouse;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                DeleteProduct(connection, "ProductToDelete");
+                DeleteSupplier(connection, "SupplierToDelete");
+                DeleteProductType(connection, "TypeToDelete");
+            }
+        }
+        static void DeleteProduct(SqlConnection connection, string productName)
+        {
+            using (SqlCommand command = new SqlCommand($"DELETE FROM Products WHERE Product_Name = '{productName}'", connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        static void DeleteSupplier(SqlConnection connection, string supplierName)
+        {
+            using (SqlCommand command = new SqlCommand($"DELETE FROM Suppliers WHERE Supplier_Name = '{supplierName}'", connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        static void DeleteProductType(SqlConnection connection, string productType)
+        {
+            using (SqlCommand command = new SqlCommand($"UPDATE Products SET Product_Type = NULL WHERE Product_Type = '{productType}'", connection))
+            {
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
