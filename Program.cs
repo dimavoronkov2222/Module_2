d3f5b0d5ecda85cb6244c39d196ebf805de7bbd0
dimavoronkov2222@hotmail.com <dimavoronkov2222@hotmail.com>
2023-12-26 19:59:51 +0100

    fds

--- Program.cs
+++ Program.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Data.SqlClient;
 namespace Module_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string connectionString = "Data Source=LAPTOP-2;Initial Catalog=Warehouse;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("The connection to the \"Warehouse\" database has been successfully established.");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Database connection error: " + ex.Message);
+                }
+            }
+            Console.WriteLine("The connection to the \"Warehouse\" database is closed.");
         }
     }
 }
